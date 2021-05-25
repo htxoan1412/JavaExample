868bd2286943dc85cbcdc2aba47b5ab8d9f97172
Huan2302 <48352794+Huan2302@users.noreply.github.com>
2021-05-25 11:39:52 +0700

    Update Lab1Bai1.java
    không dùng sc.nextDouble()
    thay vào Double.parseDouble(scanner.nextLine());

--- src/Bai1/Lab1Bai1.java
+++ src/Bai1/Lab1Bai1.java
@@ -6,9 +6,9 @@ public class Lab1Bai1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Họ và tên : ");
-        String hoTen = scanner.nextLine();
+        String hoTen = Double.parseDouble(scanner.nextLine());
         System.out.print("Điểm trung bình : ");
-        Double diemTB = scanner.nextDouble();
+        Double diemTB = Double.parseDouble(scanner.nextLine());
         System.out.printf("%s %f điểm ",hoTen,diemTB);
     }
 }
